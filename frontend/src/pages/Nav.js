c91d0dfc1d69b98b9cbf7b0b415a7b17f89ea600
Trecia Kat <tksekgalo@gmail.com>
2022-03-13 18:11:26 +0200

    fomatted navbar

---
+++ frontend/src/pages/Nav.js
@@ -0,0 +1,21 @@
+// frontend/src/pages/nav/Nav.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+    
+export default function NavHeader() {
+    return (
+        <div className="">
+          <h1>My Estate</h1>
+          <nav className="">
+            <ul>
+              <Link to="/" style={{ textDecoration: 'none' }}>
+                <li>Home</li>
+              </Link>
+              <Link to="estates" style={{ textDecoration: 'none' }}>
+                <li>Estates</li>
+              </Link>
+            </ul>
+          </nav>
+        </div>
+    );
+}
