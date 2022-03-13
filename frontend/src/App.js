39590801d9436dd82e6c7773ebae8fe9b1db4b4d
Trecia Kat <tksekgalo@gmail.com>
2022-03-13 18:11:11 +0200

    Corrected incorrect path directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// frontend/src/App.js
+import React, { Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavHeader from './pages/Nav';
+import Homepage from './pages/Home';
+    
+const About = React.lazy(() => import('./pages/About'));
+const Estates = React.lazy(() => import('./pages/Estate'));
+    
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <div>
+      <NavHeader />
+      <h1 className='text-red-500'>Hello</h1>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+            <Route
+              path="estates"
+              element={
+                <Suspense fallback={<p>Loading...</p>}>
+                    <Estates />
+                </Suspense>
+              }
+              />
+              <Route
+                path="estates/:id"
+                element={
+                  <Suspense fallback={<p>Loading...</p>}>
+                    <About />
+                  </Suspense>
+                }
+              />
+            </Routes>
+      </div>
+    )
+  }
