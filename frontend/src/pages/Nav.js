f89ec212075144f03e9c778b35dfee4039cf3170
Trecia Kat <tksekgalo@gmail.com>
2022-03-14 14:04:59 +0200

    forgot to deleted test code

--- frontend/src/pages/Nav.js
+++ frontend/src/pages/Nav.js
@@ -4,19 +4,6 @@ import { Link } from 'react-router-dom'
 export default function NavHeader() {
     return (
       <header className="text-gray-400 bg-gray-900 body-font">
-          {/* <header className="text-gray-400 bg-gray-900 body-font">
-  <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-    <div className="flex title-font font-medium items-center text-yellow-500 mb-2 md:mb-0">
-      <span className="ml-3 text-xl uppercase">Housed</span>
-    </div>
-    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-yellow-500">First Link</a>
-      <a className="mr-5 hover:text-yellow-500">Second Link</a>
-      <a className="mr-5 hover:text-yellow-500">Third Link</a>
-      <a className="mr-5 hover:text-yellow-500">Fourth Link</a>
-    </nav>
-    </div>
-</header> */}
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <div className="flex title-font font-medium items-center text-yellow-500 mb-2 md:mb-0">
             <span className="ml-3 text-xl uppercase">Housed</span>
